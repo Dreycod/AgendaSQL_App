3f900331cacbc06a393399f0d4b532c32b9a12eb
Drey <84332780+Dreycod@users.noreply.github.com>
2024-04-04 15:45:05 +0200

    test

---
+++ AgendaSQL_App/Service/DAO/DAO_Calendrier.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.Threading;
+using System.Linq;
+
+namespace WpfAgendaDatabase.Service.DAO
+{
+    public class DAO_GoogleCalendar
+    {
+        private static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        private static string ApplicationName = "AgendaDB";
+
+        // Méthode pour obtenir le service de calendrier
+        public static async Task<CalendarService> GetCalendarServiceAsync()
+        {
+            UserCredential credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = "536688738653-hhfdpq3e4at05ks63ql0r7rihav32cgr.apps.googleusercontent.com",
+                    ClientSecret = "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"
+                },
+                Scopes,
+                "user",
+                CancellationToken.None,
+                new FileDataStore("token.json", true));
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            return service;
+        }
+
+        // Méthode pour obtenir les événements du calendrier
+        public static async Task<List<Google.Apis.Calendar.v3.Data.Event>> GetEventsAsync(CalendarService service)
+        {
+            // Assurez-vous que le service est déjà initialisé
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service), "Le service de calendrier n'est pas initialisé.");
+            }
+
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            try
+            {
+                var events = await request.ExecuteAsync();
+                return events.Items.ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<Google.Apis.Calendar.v3.Data.Event>();
+            }
+        }
+    }
+}
--- AgendaSQL_App/View/Page_Agenda.xaml.cs
+++ AgendaSQL_App/View/Page_Agenda.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using AgendaSQL_App.Model;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAgendaDatabase.Service.DAO;
 
 namespace AgendaSQL_App.View
 {
@@ -20,9 +26,43 @@ namespace AgendaSQL_App.View
     /// </summary>
     public partial class Page_Agenda : UserControl
     {
+        public ObservableCollection<Event> Events { get; set; } = new ObservableCollection<Event>();
+
         public Page_Agenda()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+            LoadEventsAsync();
+        }
+
+        private async void LoadEventsAsync()
+        {
+            try
+            {
+                var service = await DAO_GoogleCalendar.GetCalendarServiceAsync();
+                var request = service.Events.List("primary");
+                request.TimeMin = DateTime.Now;
+                request.ShowDeleted = false;
+                request.SingleEvents = true;
+                request.MaxResults = 10;
+                request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+                var events = await request.ExecuteAsync();
+
+
+                foreach (var eventItem in events.Items)
+                {
+                    Events.Add(eventItem);
+                }
+
+                EventsListView.ItemsSource = Events;
+            }
+            catch (Exception ex)
+            {
+                // Gérer l'erreur, par exemple en affichant un message
+                Console.WriteLine($"Erreur lors du chargement des événements: {ex.Message}");
+            }
         }
     }
 }
